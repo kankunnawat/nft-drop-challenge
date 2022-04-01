d5bd8286a22bb9e8cb59fd8904493655ecf81aa3
kan1657 <kan1657@hotmail.com>
2022-04-01 23:14:23 +0700

    Finish Day2

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react'
 
 function NFTDropPage() {
+  // Auth
+  const connectWithMetamask = useMetamask()
+  const address = useAddress()
+  const disconnect = useDisconnect()
+
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/* Left */}
@@ -22,7 +28,46 @@ function NFTDropPage() {
       </div>
 
       {/* Right */}
-      <div className="lg:grid-cols-6"></div>
+      <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+        <header className="flex items-center justify-between ">
+          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+            The{' '}
+            <span className="font-extrabold underline decoration-pink-600/50">
+              PAPAFAM
+            </span>{' '}
+            NFT Market Place
+          </h1>
+          <button
+            onClick={() => (address ? disconnect() : connectWithMetamask())}
+            className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base"
+          >
+            {address ? 'Sign Out' : 'Sign In'}
+          </button>
+        </header>
+
+        <hr className="my-2 border" />
+        {address && (
+          <p className="text-center text-sm text-rose-400">
+            You're logged in with wallet {address.substring(0, 5)}...
+            {address.substring(address.length - 5)}
+          </p>
+        )}
+
+        <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
+          <img
+            className="w-80 object-cover pb-10 lg:h-40"
+            src="https://links.papareact.com/bdy"
+            alt=""
+          />
+          <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
+            The PAPAFAM Ape Coding Club | NFT DROP
+          </h1>
+          <p className="pt-2 text-xl text-green-500">13 / 21 NFT's Claimed</p>
+        </div>
+        <button className="mt-10 h-16 w-full rounded-full bg-red-600 font-bold text-white">
+          Mint NFT (0.01ETH)
+        </button>
+      </div>
     </div>
   )
 }
