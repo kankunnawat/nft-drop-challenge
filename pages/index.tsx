b205fac649e5f20974664bb629ea4afc50ac6093
kan1657 <kan1657@hotmail.com>
2022-04-02 14:54:58 +0700

    bug collection is null

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,88 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { sanityClient, urlFor } from '../sanity'
+import { Collection } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[]
+}
+
+const Home = ({ collections }: Props) => {
+  console.log('collections', collections)
+
   return (
     <div>
       <Head>
         <title>NFT Drop Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome</h1>
+      <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+        The{' '}
+        <span className="font-extrabold underline decoration-pink-600/50">
+          PAPAFAM
+        </span>{' '}
+        NFT Market Place
+      </h1>
+
+      <main>
+        <div>
+          {collections.map((collection) => (
+            <div className="flex flex-col items-center">
+              <img
+                className="h-96 w-60 rounded-2xl object-cover"
+                src={urlFor(collection.mainImage).url()}
+                alt=""
+              />
+
+              <div>
+                <h2 className="text-3xl">{collection.title}</h2>
+                <p className="mt-2 text-sm text-gray-400">
+                  {collection.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == "collection"]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+      asset
+    },
+    previewImage {
+      asset
+    },
+    slug {
+      current
+    },
+    creator-> {
+      _id,
+      name,
+      address,
+      slug {
+        current
+      },
+    },
+  }`
+
+  const collections = await sanityClient.fetch(query)
+  console.log('collections inside ', collections)
+
+  return {
+    props: {
+      collections,
+    },
+  }
+}
